83049be98de39fc35a199057433c633797d47944
Andreu <andreu.martinez.garacia.1992@gmail.com>
2022-02-16 13:11:22 +0100

    Creado modelo de peliculas

---
+++ migrations/20220216121028-create-pelicula.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Peliculas', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id: {
+        type: Sequelize.INTEGER
+      },
+      titulo: {
+        type: Sequelize.STRING
+      },
+      sinopsis: {
+        type: Sequelize.STRING
+      },
+      adult: {
+        type: Sequelize.BOOLEAN
+      },
+      imagen: {
+        type: Sequelize.STRING
+      },
+      fecha: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Peliculas');
+  }
+};
