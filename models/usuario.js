c4b36f786440748568e89bf6fe481149b1cb24a5
Andreu <andreu.martinez.garacia.1992@gmail.com>
2022-02-16 13:39:06 +0100

    Creado modelo de Usuarios

---
+++ models/usuario.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Usuario extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Usuario.init({
+    nombre: DataTypes.STRING,
+    apellido: DataTypes.STRING,
+    edad: DataTypes.INTEGER,
+    email: DataTypes.STRING,
+    DNI: DataTypes.STRING,
+    telefono: DataTypes.INTEGER,
+    numCuenta: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Usuario',
+  });
+  return Usuario;
+};
