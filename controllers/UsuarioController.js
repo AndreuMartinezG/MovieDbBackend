474eee4bc43466b75c7c67805696e3244804ed61
Andreu <andreu.martinez.garacia.1992@gmail.com>
2022-02-17 10:27:49 +0100

    Comenzando endpoint de USUARIOS /registro

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -8,7 +8,16 @@ UsuarioController.traeUsuarios = (req, res) => {
 };
 
 UsuarioController.registraUsuario = (req, res) => {
+    
+    let cuerpoRegistro = req.body
 
+    try {
+
+        res.send(cuerpoRegistro)
+
+    }catch(error){
+        res.send(error);
+    }
 };
 
 UsuarioController.logUsuario = (req, res) => {
