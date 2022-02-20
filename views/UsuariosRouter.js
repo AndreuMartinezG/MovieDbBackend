d2591c8d2ae27cab27bcf101556131da0ba5ebb2
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-02-20 12:33:29 +0100

    Actualizada descripcion de ENDpoints UsuariosRouter.js

--- views/UsuariosRouter.js
+++ views/UsuariosRouter.js
@@ -11,17 +11,23 @@ router.get('/', UsuarioController.traeUsuarios);
 
 //Registro
 router.post('/', UsuarioController.registraUsuario);
-//http://localhost:3000/usuarios
 
+//Borra todos los usuarios de la base de datos
 router.delete('/', UsuarioController.deleteAll)
-
-
 
 //Login
 router.post('/login', UsuarioController.logUsuario);
+
+//Busca en la db usuarios por ID
 router.get('/:id', UsuarioController.traerUsuarioId);
+
+//Borra de la db usuarios por ID
 router.delete('/:id', UsuarioController.deleteById);
+
+//Actualiza datos de usuarios en la DB
 router.put('/:id', UsuarioController.updateProfile);
+
+//Busca usuarios en la db por email
 router.get('/email/:email', UsuarioController.traerUsuarioEmail);
 
 
