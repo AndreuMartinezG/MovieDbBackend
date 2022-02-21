6d0c110f52cc2a04010d24483538de190fc6178a
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-02-21 17:22:50 +0100

    Aplicado auth a peliculasRouter.js

--- views/PeliculasRouter.js
+++ views/PeliculasRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const auth = require("../middlewares/auth");
 
 const PeliculasController = require('../controllers/PeliculasController');
 
@@ -7,35 +8,35 @@ const PeliculasController = require('../controllers/PeliculasController');
 //CRUD RESTful
 
 //Leer todos los usuarios
-router.get('/', PeliculasController.traeGeneros);
+router.get('/', auth, PeliculasController.traeGeneros);
 //http://localhost:3000/peliculas
 
 //Registro
-router.post('/', PeliculasController.registraPelicula);
+router.post('/', auth, PeliculasController.registraPelicula);
 //http://localhost:3000/peliculas
 
 
 //Busqueda de peliculas por titulo
-router.get('/titulo', PeliculasController.peliculasTitulo)
+router.get('/titulo', auth, PeliculasController.peliculasTitulo)
 
 //Busqueda de novedades
-router.get('/novedades', PeliculasController.traeNovedades)
+router.get('/novedades', auth, PeliculasController.traeNovedades)
 
 //Ultima pelicula subida a la base de datos -- /latest
-router.get('/ultimas', PeliculasController.peliculasUltimas)
+router.get('/ultimas', auth, PeliculasController.peliculasUltimas)
 
 // Traemos las peliculas con mejor nota -- /mejor_valoradas
-router.get('/top_rated', PeliculasController.peliculasValoradas)
+router.get('/top_rated', auth, PeliculasController.peliculasValoradas)
 
 
 //Traemos las peliculas relacionadas con la pelicula ID
-router.get('/:id/relacionadas', PeliculasController.peliculasRelacionadas)
+router.get('/:id/relacionadas', auth, PeliculasController.peliculasRelacionadas)
 
 //Busqueda por ID
-router.get('/:id', PeliculasController.peliculasPorId)
+router.get('/:id', auth, PeliculasController.peliculasPorId)
 
 //Busqueda de Reviews de peliculas por id
-router.get('/:id/reviews', PeliculasController.peliculasIdReviews)
+router.get('/:id/reviews', auth, PeliculasController.peliculasIdReviews)
 
 
 module.exports = router;
