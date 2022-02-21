851ff9a217c3c05b86bdc018b24827b47b86ea2a
Andreu <andreu.martinez.garacia.1992@gmail.com>
2022-02-21 10:06:30 +0100

    Creandoo PEdidos model

---
+++ models/order.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Order extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.belongsTo(models.Pelicula, {
+        foreignKey: 'peliculaId'
+      })
+      this.belongsTo(models.Usuario, {
+        foreignKey: 'usuarioId'
+      })
+    }
+  }
+  Order.init({
+    precio: DataTypes.INTEGER,
+    peliculaId: DataTypes.INTEGER,
+    usuarioID: DataTypes.INTEGER,
+    fechaEntrega: DataTypes.DATE,
+    fechaDevolucion: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Order',
+  });
+  return Order;
+};
