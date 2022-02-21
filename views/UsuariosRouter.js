ed0542fcc5a450006fda1a4f33a621f1ec8bd446
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-02-21 17:21:36 +0100

    Aplicado auth a usuarioRouter.js

--- views/UsuariosRouter.js
+++ views/UsuariosRouter.js
@@ -1,34 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const auth = require("../middlewares/auth");
 
 const UsuarioController = require('../controllers/UsuarioController');
 
 //CRUD RESTful
 
 //Leer todos los usuarios
-router.get('/', UsuarioController.traeUsuarios);
+router.get('/', auth, UsuarioController.traeUsuarios);
 //http://localhost:3000/usuarios
 
 //Registro
 router.post('/', UsuarioController.registraUsuario);
 
 //Borra todos los usuarios de la base de datos
-router.delete('/', UsuarioController.deleteAll)
+router.delete('/', auth, UsuarioController.deleteAll)
 
 //Login
 router.post('/login', UsuarioController.logUsuario);
 
 //Busca en la db usuarios por ID
-router.get('/:id', UsuarioController.traerUsuarioId);
+router.get('/:id', auth, UsuarioController.traerUsuarioId);
 
 //Borra de la db usuarios por ID
-router.delete('/:id', UsuarioController.deleteById);
+router.delete('/:id', auth, UsuarioController.deleteById);
 
 //Actualiza datos de usuarios en la DB
-router.put('/:id', UsuarioController.updateProfile);
+router.put('/:id', auth, UsuarioController.updateProfile);
 
 //Busca usuarios en la db por email
-router.get('/email/:email', UsuarioController.traerUsuarioEmail);
+router.get('/email/:email', auth, UsuarioController.traerUsuarioEmail);
 
 
 
