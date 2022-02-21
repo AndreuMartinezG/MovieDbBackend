cef82a8a16b6e86c486d78ece7fef0bb6d17f8b5
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-02-21 19:46:25 +0100

    Pruebas Auth fallidas, retornamos a previo commit

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,18 +1,17 @@
 const jwt = require('jsonwebtoken');
 const authConfig = require('../config/auth');
-const { Usuario } = require('../models/index');
 
 module.exports = (req, res, next) => {
 
-    console.log(req.Usuario.token);
+    console.log(req.headers);
 
     // Comprobar que existe el token
-    if (!req.Usuario.token) {
+    if (!req.headers.authorization) {
         res.status(401).json({ msg: "Acceso no autorizado" });
     } else {
 
         // Comrpobar la validez de este token
-        let token = req.Usuario.token;
+        let token = req.headers.authorization.split(" ")[1];
 
         // Comprobar la validez de este token
         jwt.verify(token, authConfig.secret, (err, decoded) => {
