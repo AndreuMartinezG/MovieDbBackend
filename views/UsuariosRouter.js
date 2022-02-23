d71872fbddecc2fc0c91900705b85f607bcd4187
Andreu <andreu.martinez.garacia.1992@gmail.com>
2022-02-23 09:34:45 +0100

    Añadido isAdmin middleware a /views/usuariosRouter.js

--- views/UsuariosRouter.js
+++ views/UsuariosRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require("../middlewares/auth");
-
+const isAdmin = require("../middlewares/isAdmin");
 const UsuarioController = require('../controllers/UsuarioController');
 
 //CRUD RESTful
